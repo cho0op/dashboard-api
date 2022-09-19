3686cd01651419d039f7fd1ebebfd0ceb92478e1
cho0op <sorintema@gmail.com>
2022-09-19 15:16:19 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = 8000;
+const app = express();
+
+
+app.get("/hello", (req, res) => {
+    res.download(".gitignore");
+});
+
+app.listen(port, () => {
+});
+
