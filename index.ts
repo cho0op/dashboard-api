5080699c1a35670a5be5e82b47a0645cd5dabc7d
Artsiom <sorintema@gmail.com>
2022-09-24 11:59:05 +0300

    Added ts

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
 import express from "express";
+import { userRouter } from "./users/users.js";
 
 const port = 8000;
 const app = express();
 
 
-app.get("/hello", (req, res) => {
-    res.download(".gitignore");
-});
+app.use("/users", userRouter);
 
 app.listen(port, () => {
 });
-
 
