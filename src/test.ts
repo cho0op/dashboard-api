2c1e0f8de19da4622450f227df1170511dc9a18e
Artsiom <sorintema@gmail.com>
2022-10-25 19:00:04 +0300

    Added reflect-metadata

--- src/test.ts
+++ src/test.ts
@@ -1,45 +1,11 @@
-const Component = (id: number) => {
-    return (target: Function) => {
-        target.prototype.id = id;
-    };
-};
+import "reflect-metadata";
 
-function Method(target: Object, propertyKey: string, propertyDescriptor: PropertyDescriptor) {
-    propertyDescriptor.value = function (...args: any[]) {
-        return args[0] * 10;
-    };
+function Test(target: Function) {
+    Reflect.defineMetadata("a", 1, target);
+    const meta = Reflect.getMetadata("a", target);
+    console.log(meta);
 }
 
-function Prop(target: Object, propertyKey: string) {
-    let value: number;
-
-    const getter = () => {
-        return value;
-    };
-    const setter = (newValue: number) => {
-        value = newValue;
-    };
-
-    Object.defineProperty(target, propertyKey, {
-        get: getter,
-        set: setter
-    });
-
+@Test
+export class Class {
 }
-
-function Param(target: Object, propertyKey: string, index: number) {
-
-}
-
-@Component(5)
-export class User {
-    @Prop
-    id: number;
-
-    updateId(newId: number) {
-        this.id = newId;
-        return this.id;
-    };
-}
-
-console.log(new User().id);
