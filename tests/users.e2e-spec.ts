dddde3a336f20b91a4c408ace33b27dc85854c75
cho0op <sorintema@gmail.com>
2023-04-17 20:00:11 +0300

    Added e2e test

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -18,6 +18,37 @@ describe("Users e2e", () => {
 		});
 		expect(response.status).toBe(422);
 	});
+	it("login - error", async () => {
+		const response = await request(application.app).post("/users/login").send({
+			password: "asdas@asd.ci123123",
+			email: "asdas@asd.ci",
+		});
+		expect(response.status).toBe(403);
+	});
+	it("login - success", async () => {
+		const response = await request(application.app).post("/users/login").send({
+			password: "asdas@asd.ci",
+			email: "asdas@asd.ci",
+		});
+		expect(response.status).toBe(200);
+	});
+	it("info - error", async () => {
+		const response = await request(application.app).get("/users/info").send();
+		expect(response.status).toBe(401);
+	});
+	it("info - success", async () => {
+		const { body } = await request(application.app).post("/users/login").send({
+			password: "asdas@asd.ci",
+			email: "asdas@asd.ci",
+		});
+		console.log(body.jwt);
+		const response = await request(application.app)
+			.get("/users/info")
+			.set("Authorization", `Token ${body.jwt}`)
+			.send();
+
+		expect(response.status).toBe(200);
+	});
 });
 
 afterAll(() => {
